d6cd92b2bc7368dd7db0107307329a01a674be0c
Theophilus <60826700+Extheoisah@users.noreply.github.com>
2024-03-08 21:35:23 +0100

    fix: change access permissions for next auth github provider (#12)

--- client/fresheyes-web/src/app/api/auth/[...nextauth]/options.ts
+++ client/fresheyes-web/src/app/api/auth/[...nextauth]/options.ts
@@ -1,38 +1,40 @@
-import NextAuth, { GhExtendedProfile, NextAuthOptions, Session } from "next-auth"
-import { JWT } from "next-auth/jwt"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth, {
+  GhExtendedProfile,
+  NextAuthOptions,
+  Session,
+} from "next-auth";
+import { JWT } from "next-auth/jwt";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        })
-    ],
-    callbacks: {
-        async jwt({ token, account, profile }) {
-            if (account) {
-                token.accessToken = account.access_token
-                token.profile = profile
-            }
-            return token
-        },
-        async session({
-            session,
-            token,
-        }: {
-            session: Session
-            token: JWT
-        }) {
-            if (token.accessToken) {
-                session.accessToken = token.accessToken as string
-                const { login, html_url } = token.profile as GhExtendedProfile
-                session.user.login = login
-                session.user.html_url = html_url
-            }
-            return session
-        }
-    }
-}
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        url: "https://github.com/login/oauth/authorize",
+        params: { scope: "read:user user:email public_repo" },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.profile = profile;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: Session; token: JWT }) {
+      if (token.accessToken) {
+        session.accessToken = token.accessToken as string;
+        const { login, html_url } = token.profile as GhExtendedProfile;
+        session.user.login = login;
+        session.user.html_url = html_url;
+      }
+      return session;
+    },
+  },
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
