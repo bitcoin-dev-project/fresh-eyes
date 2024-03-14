0ffcd7f5f0f4e2dac4ae70b8ada166b15f27d243
IgboPharaoh <solomoneze62@gmail.com>
2024-03-14 15:42:43 +0100

    modify body text and sort data set

--- client/fresheyes-bot/src/util.ts
+++ client/fresheyes-bot/src/util.ts
@@ -1,5 +1,16 @@
+type Comment = {
+  body: string;
+  commit_id?: string;
+  path?: string;
+  side?: "LEFT" | "RIGHT" | undefined;
+  line?: number;
+  created_at: string;
+  key: string;
+};
+
 export function groupCommentsFn<T extends Array<Record<string, any>>>(data: T) {
   const comments: Record<string, Array<typeof data>> = data
+    .filter((f) => f.line !== null)
     .map((x: any) => ({ ...x, line: String(x.line) }))
     .reduce((acc, curr) => {
       const key = curr.line;
@@ -12,7 +23,7 @@ export function groupCommentsFn<T extends Array<Record<string, any>>>(data: T) {
   return comments;
 }
 
-export function generateBody<T extends Array<Array<Record<string, any>>>>(value: T) {
+export function getReviewBody<T extends Array<Array<Record<string, any>>>>(value: T) {
   const list = value.flat().map((x) => ({ html_url: x.html_url, created_at: x.created_at }));
 
   const formatString = list
@@ -22,7 +33,52 @@ export function generateBody<T extends Array<Array<Record<string, any>>>>(value:
     .join("\n");
 
   const body = `${value.length === 1 ? "An author" : `${value.length} authors`} commented here with\n\n${formatString}.`;
-  const comment = value.flat()[0];
+
+  const comment = value.flat().sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime())[0];
 
   return { body, comment };
+}
+
+export function getIssueBody<T extends Pick<T, "html_url" | "created_at">>(arg: T, idx: number) {
+  const formatString = `- [comment link ${idx + 1}](${arg.html_url}) at ${new Date(arg.created_at as string | Date).toLocaleString()}`;
+
+  const body = `An author commented here with\n\n${formatString}.`;
+
+  return { body };
+}
+
+export function extractData<R extends Array<Record<string, any>>, I extends Array<Pick<Record<string, any>, "html_url" | "created_at">>>(
+  reviews: R,
+  issues: I
+) {
+  const groupReviews = groupCommentsFn(reviews);
+
+  const extract_reviews = Object.entries(groupReviews).map(([key, value]) => {
+    const { body, comment } = getReviewBody(value);
+
+    return {
+      body: body,
+      commit_id: comment.commit_id,
+      path: comment.path,
+      side: comment.side,
+      line: Number(key),
+      created_at: comment.created_at,
+      key: "review",
+    };
+  });
+
+  const extract_issues = issues.map((i, idx) => {
+    const { body } = getIssueBody(i, idx);
+    return {
+      body,
+      created_at: i.created_at,
+      key: "issue",
+    };
+  });
+
+  const allComments: Comment[] = [...extract_reviews, ...extract_issues].sort(
+    (a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+  );
+
+  return { allComments };
 }
